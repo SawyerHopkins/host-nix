e3bc4ee5ae43a445465afedaa6e1eae8e4e2cda7
Sawyer Hopkins <sawyer@sawyerhopkins.com>
2026-03-31 23:26:28 -0700

    fix home manager issue

--- dot_files/code.nix
+++ dot_files/code.nix
@@ -1,11 +1,6 @@
 {
-  "Code/shell.nix" = {
-    source = ./code.d/shell.nix;
-  };
-  "Code/.envrc" = {
-    source = ./code.d/envrc;
-  };
-  "Code/.scripts" = {
-    source = ./code.d/scripts;
+  "Code" = {
+    source = ./code.d;
+    recursive = true;
   };
 }
